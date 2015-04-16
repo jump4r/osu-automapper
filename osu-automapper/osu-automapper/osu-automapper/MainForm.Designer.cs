174c1fd6f46e3ac73c0798de5795106c4fdabec6
Colton Wicks <11cwicks@gmail.com>
2015-04-16 13:22:56 -0400

    Cleaned up code.

--- osu-automapper/osu-automapper/osu-automapper/MainForm.Designer.cs
+++ osu-automapper/osu-automapper/osu-automapper/MainForm.Designer.cs
@@ -29,11 +29,9 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.pauseButton = new System.Windows.Forms.Button();
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
             this.waveViewer1 = new NAudio.Gui.WaveViewer();
             this.createRandomButton = new System.Windows.Forms.Button();
-            this.createButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pathLabel = new System.Windows.Forms.Label();
@@ -41,26 +39,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.creatButton = new System.Windows.Forms.PictureBox();
+            this.playButton = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.creatButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playButton)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pauseButton
-            // 
-            this.pauseButton.BackColor = System.Drawing.Color.White;
-            this.pauseButton.Location = new System.Drawing.Point(691, 423);
-            this.pauseButton.Margin = new System.Windows.Forms.Padding(5);
-            this.pauseButton.Name = "pauseButton";
-            this.pauseButton.Size = new System.Drawing.Size(313, 38);
-            this.pauseButton.TabIndex = 1;
-            this.pauseButton.Text = "Pause/Play MP3";
-            this.pauseButton.UseVisualStyleBackColor = false;
-            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
             // 
             // fileSystemWatcher1
             // 
@@ -86,7 +72,7 @@
             // createRandomButton
             // 
             this.createRandomButton.BackColor = System.Drawing.Color.White;
-            this.createRandomButton.Location = new System.Drawing.Point(691, 331);
+            this.createRandomButton.Location = new System.Drawing.Point(725, 453);
             this.createRandomButton.Margin = new System.Windows.Forms.Padding(5);
             this.createRandomButton.Name = "createRandomButton";
             this.createRandomButton.Size = new System.Drawing.Size(313, 34);
@@ -94,18 +80,6 @@
             this.createRandomButton.Text = "Create Random Beatmap";
             this.createRandomButton.UseVisualStyleBackColor = false;
             this.createRandomButton.Click += new System.EventHandler(this.createRandomButton_Click);
-            // 
-            // createButton
-            // 
-            this.createButton.BackColor = System.Drawing.Color.White;
-            this.createButton.Location = new System.Drawing.Point(691, 376);
-            this.createButton.Margin = new System.Windows.Forms.Padding(5);
-            this.createButton.Name = "createButton";
-            this.createButton.Size = new System.Drawing.Size(313, 37);
-            this.createButton.TabIndex = 3;
-            this.createButton.Text = "Create Audio Beatmap";
-            this.createButton.UseVisualStyleBackColor = false;
-            this.createButton.Click += new System.EventHandler(this.createButton_Click);
             // 
             // pictureBox1
             // 
@@ -116,7 +90,7 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Click += new System.EventHandler(this.openFileButton_Click);
             // 
             // pictureBox2
             // 
@@ -167,28 +141,28 @@
             this.label3.Size = new System.Drawing.Size(98, 65);
             this.label3.TabIndex = 11;
             // 
-            // pictureBox3
+            // creatButton
             // 
-            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(25, 246);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(194, 178);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 12;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.creatButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.creatButton.Image = ((System.Drawing.Image)(resources.GetObject("creatButton.Image")));
+            this.creatButton.Location = new System.Drawing.Point(25, 246);
+            this.creatButton.Name = "creatButton";
+            this.creatButton.Size = new System.Drawing.Size(194, 178);
+            this.creatButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.creatButton.TabIndex = 12;
+            this.creatButton.TabStop = false;
+            this.creatButton.Click += new System.EventHandler(this.createButton_Click);
             // 
-            // pictureBox4
+            // playButton
             // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(235, 246);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(194, 178);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox4.TabIndex = 13;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.playButton.Image = ((System.Drawing.Image)(resources.GetObject("playButton.Image")));
+            this.playButton.Location = new System.Drawing.Point(235, 246);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(194, 178);
+            this.playButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.playButton.TabIndex = 13;
+            this.playButton.TabStop = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
             // 
             // MainForm
             // 
@@ -196,16 +170,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(51)))), ((int)(((byte)(143)))));
             this.ClientSize = new System.Drawing.Size(1052, 501);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.creatButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.blockerLabel1);
             this.Controls.Add(this.pathLabel);
             this.Controls.Add(this.createRandomButton);
-            this.Controls.Add(this.createButton);
-            this.Controls.Add(this.pauseButton);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.waveViewer1);
@@ -216,18 +188,16 @@
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.creatButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playButton)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button pauseButton;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
         private NAudio.Gui.WaveViewer waveViewer1;
-        private System.Windows.Forms.Button createButton;
         private System.Windows.Forms.Button createRandomButton;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
@@ -236,8 +206,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label blockerLabel1;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox creatButton;
+        private System.Windows.Forms.PictureBox playButton;
 
     }
 }
--- osu-automapper/osu-automapper/osu-automapper/MainForm.cs
+++ osu-automapper/osu-automapper/osu-automapper/MainForm.cs
@@ -45,36 +45,6 @@ namespace osu_automapper
 			button.BackgroundImage = bmp;
 		}
 
-		private void pauseButton_Click(object sender, EventArgs e)
-		{
-            if (this.mp3FilePath == null || this.mp3FilePath == "")
-                return;
-
-            if (output != null)
-			{
-				if (output.PlaybackState == PlaybackState.Playing)
-				{
-					output.Pause();
-                    Console.WriteLine("Paused");
-				}
-				else if (output.PlaybackState == PlaybackState.Paused)
-				{
-					output.Play();
-                    Console.WriteLine("Played");
-				}
-			}
-
-            if(output == null)
-            {
-                WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(this.mp3FilePath));
-                waveViewer1.WaveStream = pcm;
-                stream = new BlockAlignReductionStream(pcm);
-                output = new DirectSoundOut();
-                output.Init(stream);
-                output.Play();
-            }
-		}
-
 		private void DisposeWave()
 		{
 			if (output != null)
@@ -91,41 +61,6 @@ namespace osu_automapper
 			}
 		}
 
-		private void openOSUFile_Click(object sender, EventArgs e)
-		{
-            
-			var open = new OpenFileDialog();
-			open.Filter = "OSU file (*.osu)|*.osu;";
-
-			if (open.ShowDialog() != DialogResult.OK) { return; }
-            Console.WriteLine("Opening .osb/.mp3 files...");
-
-			// If path is good, lets get the file contents.     
-			if (!open.CheckPathExists)
-			{
-				Console.WriteLine("Error: Path does not exist.");
-				return;
-			}
-
-            string filename = open.FileName;
-			this.beatmap = new Beatmap(filename);
-            string path = Path.GetDirectoryName(filename);
-            string mp3Name = this.beatmap.mp3Name;
-            this.mp3FilePath = Path.Combine(path, mp3Name);
-            this.pathLabel.Text = path;
-            if(mp3Name == "" || path == "")
-            {
-                Console.WriteLine("Error: Could not find mp3 file.");
-            }
-            else
-            {
-                Console.WriteLine("complete.");
-
-                WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(this.mp3FilePath));
-                waveViewer1.WaveStream = pcm;
-            }
-		}
-
 		private void createRandomButton_Click(object sender, EventArgs e)
 		{
 			if (beatmap == null)
@@ -134,27 +69,6 @@ namespace osu_automapper
 			}
 
 			beatmap.CreateRandomBeatmap();
-		}
-
-		private void createButton_Click(object sender, EventArgs e)
-		{
-			if (this.mp3FilePath == "" || beatmap == null)
-			{
-				if (this.mp3FilePath == "")
-				{
-					Console.WriteLine("Error:No MP3 selected.");
-				}
-
-				if (beatmap == null)
-				{
-					Console.WriteLine("Error:No beatmap selected.");
-				}
-
-				return;
-			}
-
-			this.analyzer = new AudioAnalyzer(this.mp3FilePath, this.beatmap);
-			this.beatmap.CreateRandomBeatmap(this.analyzer);
 		}
 
 		private void waveViewer1_MouseDown(object sender, MouseEventArgs e)
@@ -185,25 +99,94 @@ namespace osu_automapper
 			draggingWaveForm = false;
 		}
 
-        private void pictureBox3_Click_1(object sender, EventArgs e)
-        {
-            openOSUFile_Click(sender, e);
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void openFileButton_Click(object sender, EventArgs e)
         {
             //Console.WriteLine("IMAGE CICKED");
-            openOSUFile_Click(sender, e);
+
+            var open = new OpenFileDialog();
+            open.Filter = "OSU file (*.osu)|*.osu;";
+
+            if (open.ShowDialog() != DialogResult.OK) { return; }
+            Console.WriteLine("Opening .osb/.mp3 files...");
+
+            // If path is good, lets get the file contents.     
+            if (!open.CheckPathExists)
+            {
+                Console.WriteLine("Error: Path does not exist.");
+                return;
+            }
+
+            string filename = open.FileName;
+            this.beatmap = new Beatmap(filename);
+            string path = Path.GetDirectoryName(filename);
+            string mp3Name = this.beatmap.mp3Name;
+            this.mp3FilePath = Path.Combine(path, mp3Name);
+            this.pathLabel.Text = path;
+            if (mp3Name == "" || path == "")
+            {
+                Console.WriteLine("Error: Could not find mp3 file.");
+            }
+            else
+            {
+                Console.WriteLine("complete.");
+
+                WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(this.mp3FilePath));
+                waveViewer1.WaveStream = pcm;
+            }
         }
 
-        private void pictureBox3_Click(object sender, EventArgs e)
+        private void createButton_Click(object sender, EventArgs e)
         {
-            createButton_Click(sender, e);
+            if (this.mp3FilePath == "" || beatmap == null)
+            {
+                if (this.mp3FilePath == "")
+                {
+                    Console.WriteLine("Error:No MP3 selected.");
+                }
+
+                if (beatmap == null)
+                {
+                    Console.WriteLine("Error:No beatmap selected.");
+                }
+
+                return;
+            }
+
+            this.analyzer = new AudioAnalyzer(this.mp3FilePath, this.beatmap);
+            this.beatmap.CreateRandomBeatmap(this.analyzer);
         }
 
-        private void pictureBox4_Click(object sender, EventArgs e)
+        private void playButton_Click(object sender, EventArgs e)
         {
-            pauseButton_Click(sender, e);
+            if (this.mp3FilePath == null || this.mp3FilePath == "")
+                return;
+
+            if (output != null)
+            {
+                if (output.PlaybackState == PlaybackState.Playing)
+                {
+                    output.Pause();
+                    Console.WriteLine("Paused");
+                }
+                else if (output.PlaybackState == PlaybackState.Paused)
+                {
+                    output.Play();
+                    Console.WriteLine("Played");
+                }
+            }
+
+            if (output == null)
+            {
+                WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(this.mp3FilePath));
+                if (pcm == null)
+                    return;
+
+                waveViewer1.WaveStream = pcm;
+                stream = new BlockAlignReductionStream(pcm);
+                output = new DirectSoundOut();
+                output.Init(stream);
+                output.Play();
+            }
         }
 	}
 }
